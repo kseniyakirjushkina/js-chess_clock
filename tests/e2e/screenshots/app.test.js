ddfeb80f23a3358ba86c3a12d6f8f2ebf4c07b32
kseniyakirjushkina <116095883+kseniyakirjushkina@users.noreply.github.com>
2025-10-27 15:58:04 +0300

    Delete tests/e2e directory

--- tests/e2e/screenshots/app.test.js
+++
@@ -1,8 +0,0 @@
-// import { test, expect } from '@playwright/test'
-
-// test('базовый E2E тест', async ({ page }) => {
-//   await expect(page).toHaveTitle('HW0') // Заглушка - измените под вашу домашнюю страницу
-//   // Пример проверки наличия элемента
-//   // const header = page.locator('h1')
-//   // await expect(header).toBeVisible()
-// })
